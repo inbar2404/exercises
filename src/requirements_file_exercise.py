9aabd3b8f4a2b5416f8f8dea0527851da545df95
inbar <inbar2404@gmail.com>
2022-01-12 22:09:01 +0200

    many fixes

--- src/requirements_file_exercise.py
+++ src/requirements_file_exercise.py
@@ -3,45 +3,50 @@ import ez_yaml
 # TODO: Create requirements.txt
 
 
-def find_dict(main_dict: dict, to_add):
-    dict_to_compare = to_add
+def get_dict_from_data(data) -> dict:
+    dict_to_compare = data
     # TODO: Find pretty way to do that
-    if isinstance(to_add, list):
-        dict_to_compare = to_add[0]
+    if isinstance(data, list):
+        dict_to_compare = data[0]
+    return dict_to_compare
+
+
+def merge_to_main_dict(main_dict: dict, data):
+    dict_to_compare: dict = get_dict_from_data(data)
     if main_dict.keys() == dict_to_compare.keys():
-        if isinstance(to_add, list):
-            # TODO: Find pretty way to do that
-            full_list = [main_dict]
-            full_list.extend(to_add)
-            return full_list
-        return [main_dict, to_add]
+        if isinstance(data, list):
+            return [main_dict]+data
+        return [main_dict, data]
+    return None
+
+
+def merge_to_dict(main_dict: dict, data_to_add) -> list:
+    data_after_merge = merge_to_main_dict(main_dict, data_to_add)
+    if data_after_merge is not None:
+        return data_after_merge
 
     for key, value in main_dict.items():
         if isinstance(value, list) and len(value) > 0:
             value = value[0]
         if isinstance(value, dict):
-            result = find_dict(value, to_add)
-            if result is not None:
-                main_dict[key] = result
+            data_after_merge = merge_to_dict(value, data_to_add)
+            if data_after_merge is not None:
+                main_dict[key] = data_after_merge
                 return main_dict
-
     return None
 
 
-# TODO: Should I get both yamls as files?
-# TODO: Fix conventions
-def merge_yamls(main_yaml_file: str, yaml_configuration_to_add_file: str):
+def merge_yamls(main_yaml_file_path: str, configuration_to_add: str):
     # TODO: Handle case files not found?
-    with open(main_yaml_file) as fp:
-        main_yaml = yaml.full_load(fp)
-    with open(yaml_configuration_to_add_file) as fp:
-        yaml_to_add = yaml.full_load(fp)
+    with open(main_yaml_file_path) as file:
+        # TODO: Add close file - read maybe thats happen auto because '... as file'
+        main_yaml = yaml.full_load(file)
+    yaml_to_add = yaml.safe_load(configuration_to_add)
 
-    result = find_dict(main_yaml, yaml_to_add)
+    result = merge_to_dict(main_yaml, yaml_to_add)
     if result is None:
         # TODO: Handle here case that 'yaml_to_add' is list!!!
         main_yaml.update(yaml_to_add)
         result = main_yaml
 
-    # TODO: It will be better update the original main yaml file
-    ez_yaml.to_file(result, file_path='finaly.yaml')
+    ez_yaml.to_file(result, file_path=main_yaml_file_path)
