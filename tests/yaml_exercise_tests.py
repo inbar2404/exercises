b9018c836f6d7af793bb32ed001e5002d8133c5a
inbar <inbar2404@gmail.com>
2022-01-13 01:32:47 +0200

    add tests file

---
+++ tests/yaml_exercise_tests.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class YamlExerciseTest(unittest.TestCase):
+    # TODO: Implement tests cases
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+# TODO: Remove
+if __name__ == '__main__':
+    unittest.main()
